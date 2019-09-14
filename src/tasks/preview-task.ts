263d2a278575a8779bca647acf648bbb1cf8a44f
sxend <arimitsu.k+github@gmail.com>
2019-09-14 20:37:39 +0900

    add preview mode skip

--- src/tasks/preview-task.ts
+++ src/tasks/preview-task.ts
@@ -1,3 +1,14 @@
 import { Model } from '../model';
+import { getGlobal } from '../utils/objects';
 
-export function previewTask(model: Model) {}
+const __global = getGlobal();
+
+export function previewTask(model: Model): void {
+    if (
+        __global.navigator &&
+        __global.navigator.loadPurpose &&
+        __global.navigator.loadPurpose === 'preview'
+    ) {
+        throw 'abort task by previewTask';
+    }
+}
